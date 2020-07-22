bdd8c3d7b4c48905c81925f84e2c49f0c44eb597
sansheng <807156237@qq.com>
2020-07-22 21:37:01 +0800

    oo [colin] feature : 创建Person对象，包含name，age属性。提供了getName，getAge,introduce方法

--- src/test/java/practice06/Practice06Test.java
+++ src/test/java/practice06/Practice06Test.java
@@ -1,24 +1,24 @@
-//package practice06;
-//
-//import org.junit.Test;
-//
-//import static org.fest.assertions.api.Assertions.assertThat;
-//
-//public class Practice06Test {
-//    @Test
-//    public void should_person_have_name_and_age() throws Exception {
-//        Person person = new Person("Tom", 21);
-//        assertThat(person.getName()).isEqualTo("Tom");
-//        assertThat(person.getAge()).isEqualTo(21);
-//    }
-//
-//    @Test
-//    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
-//        Person tom = new Person("Tom", 21);
-//        String introduce = tom.introduce();
-//        assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
-//    }
-//
+package practice06;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class Practice06Test {
+    @Test
+    public void should_person_have_name_and_age() throws Exception {
+        Person person = new Person("Tom", 21);
+        assertThat(person.getName()).isEqualTo("Tom");
+        assertThat(person.getAge()).isEqualTo(21);
+    }
+
+    @Test
+    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
+        Person tom = new Person("Tom", 21);
+        String introduce = tom.introduce();
+        assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
+    }
+
 //    @Test
 //    public void should_student_have_name_age_and_class_number() throws Exception {
 //        Student tom = new Student("Tom", 21, 2);
@@ -52,4 +52,4 @@
 //        Teacher tom = new Teacher("Tom", 21);
 //        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.");
 //    }
-//}
+}
