91b5e4aafce72497d4ab050a59278770ebe138b8
sansheng <807156237@qq.com>
2020-07-22 23:36:12 +0800

    oo [colin] feature : 将teacher类中的klass变为classes数组链表，并添加isTeaching方法判断教该学生

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,43 @@
 package practice10;
 
 public class Klass {
+
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass() != this){
+            System.out.print("It is not one of us.\n");
+        }else {
+            this.leader = student;
+        }
+    }
+
+    public Student appendMember(Student student){
+        student.setKlass(this);
+        return student;
+    }
+
+    public boolean isIn(Student student,int number){
+        if(student.getKlass().number == number){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,52 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        if(classes == null || classes.size() == 0){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        StringBuilder sb = new StringBuilder(super.introduce() + " I am a Teacher. I teach Class ");
+        for(int i = 0; i < classes.size() - 1; i++){
+            //2, 3
+            sb.append(classes.get(i).getNumber() + ", ");
+        }
+        sb.append(classes.get(classes.size() - 1).getNumber()+".");
+        return sb.toString();
+    }
+
+    public String introduceWith(Student student){
+        for(int i = 0; i < classes.size(); i++){
+            if(classes.get(i).getNumber() == student.getKlass().getNumber()){
+                return super.introduce() + " I am a Teacher. I teach "+student.name+".";
+            }
+        }
+        return super.introduce() + " I am a Teacher. I don't teach Jerry.";
+    }
+
+    public boolean isTeaching(Student student){
+        for(int i = 0; i < classes.size(); i++){
+            if(classes.get(i).isIn(student,classes.get(i).getNumber())){
+                return true;
+            }
+        }
+        return false;
+    }
 }
