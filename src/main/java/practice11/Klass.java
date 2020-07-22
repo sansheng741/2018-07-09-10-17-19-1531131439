7965eead6de18ecee23feb8666b0ac20c4e18470
sansheng <807156237@qq.com>
2020-07-22 23:54:23 +0800

    oo [colin] feature : 学生添加到班级，老师打印相应语句

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,47 @@
 package practice11;
 
-public class Klass {
+import java.util.Observable;
+
+public class Klass extends Observable {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass() != this){
+            System.out.print("It is not one of us.\n");
+        }else {
+            this.leader = student;
+        }
+    }
+
+    public Student appendMember(Student student){
+        student.setKlass(this);
+        setChanged(); //表示目标对象已经做了修改
+        notifyObservers(student);
+        return student;
+    }
+
+    public boolean isIn(Student student,int number){
+        if(student.getKlass().number == number){
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,27 @@
 package practice11;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id,String name, int age, Klass klass) {
+        super(id,name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        if(klass.getLeader() != null && klass.getLeader().id == this.id){
+            return super.introduce() + " I am a Student. I am Leader of Class "+this.klass.getNumber()+".";
+        }
+        return super.introduce() + " I am a Student. I am at Class "+this.klass.getNumber()+".";
+    }
 }
