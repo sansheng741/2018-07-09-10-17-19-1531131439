493065337998accbb84a03981d6fa7720e7262ef
sansheng <807156237@qq.com>
2020-07-22 21:40:44 +0800

    oo [colin] feature : 创建Teacher对象，提供name,age,klass属性，提供introduce方法,判断klass是否为空并做出相应的输出。

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(null == this.klass){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class "+this.klass+".";
+    }
 }
