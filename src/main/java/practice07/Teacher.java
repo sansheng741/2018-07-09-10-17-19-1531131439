6fff5f9cc94887144c4d59c37c240a354c554408
sansheng <807156237@qq.com>
2020-07-22 21:55:57 +0800

    oo [colin] feature : 将klass抽取出来建立一个新类，它包含了number属性。Teacher中新增了introduceWith方法去判断老师是否教对应的学生

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,35 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(null == this.klass){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class "+this.klass.getNumber()+".";
+    }
+
+    public String introduceWith(Student student){
+        if(this.klass.getNumber() == student.getKlass().getNumber()){
+            //My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.
+            return super.introduce() + " I am a Teacher. I teach "+student.name+".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach Jerry.";
+    }
 }
