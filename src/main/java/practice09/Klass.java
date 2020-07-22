3eab71e4e05d5ed2c6ea66a7c053722d9ae82a1c
sansheng <807156237@qq.com>
2020-07-22 22:56:12 +0800

    oo [colin] feature : 给klass添加了一个appendMember方法，该方法可以将Student加入到该班级

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,35 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass() != this){
+            System.out.print("It is not one of us.\n");
+        }else {
+            this.leader = student;
+        }
+    }
+
+    public Student appendMember(Student student){
+        student.setKlass(this);
+        return student;
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,33 @@
 package practice09;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(int id,String name,int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == null || obj == null){
+            return false;
+        }
+        return this.id == ((Person)obj).id;
+    }
 }
